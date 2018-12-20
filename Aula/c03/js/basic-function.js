f90b13dc694017eefa7b41e52eaf568160b52f68
kobori <ubiratan.jp@outlook.com>
2018-12-21 08:00:28 +0900

    Aula-c03

---
+++ Aula/c03/js/basic-function.js
@@ -0,0 +1,9 @@
+var msg = 'Inscreva-se para receber nossa newsletter por 10% de desconto!';
+
+function updateMessage() {
+  var el = document.getElementById('message');
+  el.textContent = msg;
+}
+
+updateMessage();
+ 
---
+++ Testes/test-02/js/teste.js
@@ -0,0 +1,6 @@
+var mens = 'Aprendendo ';
+var mens2 = 'JavaScript';
+var mensagem = mens + mens2;
+
+var elMens = document.getElementById('demo');
+elMens.textContent = mensagem + ' & Node JS';
